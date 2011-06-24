679b3548507a9e55537e604ea18256bc6fa1cf73
Tim Baumann <tim@timbaumann.info>
2011-06-24 02:10:46 +0200

    yi indentation

--- .yi/yi.hs
+++ .yi/yi.hs
@@ -1,16 +1,27 @@
+module Yi.Config.Users.TimB () where
+
+import Prelude (map, lines)
 import Yi
 import Yi.Prelude
 import Yi.UI.Vty (start)
 
-myConfig = defaultVimConfig
-  { startFrontEnd = start
-  , configUI = myConfigUI
+myModeHook m = m
+  { modeIndentSettings = IndentSettings { expandTabs = True
+                                        , shiftWidth = 2
+                                        , tabSize    = 2
+                                        }
   }
 
 myConfigUI = (configUI defaultConfig)
   { configFontSize = Nothing -- 'Just 10' for specifying the size.
   --, configTheme = configTheme defaultUIConfig, -- e.g. darkBlueTheme
   , configWindowFill = ' ' -- '~' - Typical for Vim
+  }
+
+myConfig = defaultVimConfig
+  { startFrontEnd = start
+  , modeTable = fmap (onMode myModeHook) (modeTable defaultVimConfig)
+  , configUI = myConfigUI
   }
 
 main :: IO ()
