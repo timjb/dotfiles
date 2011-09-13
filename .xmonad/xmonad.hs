fb2a7642a9372824f6a112dc0bd286eb4b6585b9
Tim Baumann <tim@timbaumann.info>
2011-09-13 13:44:03 +0200

    XMonad: more workspaces, use function keys

--- .xmonad/xmonad.hs
+++ .xmonad/xmonad.hs
@@ -4,20 +4,32 @@ import XMonad.Hooks.ManageDocks
 import XMonad.Layout.IM
 import XMonad.Layout.PerWorkspace
 import XMonad.Layout.Reflect
+import XMonad.Util.EZConfig
+import qualified XMonad.StackSet as W
 
-myWorkspaces = ["1:code", "2:web"] ++ map show [3..6] ++ ["7:video"] ++ ["8:music"] ++ ["9:gimp"]
+myWorkspaces = ["1:code", "2:web"] ++ map show [3..8] ++ ["9:game","a:gimp","b:chat","c:media"]
 
 myManageHook = composeAll
-  [ className =? "Gimp" --> doShift "9:gimp"
-  , className =? "Banshee Media Player" --> doShift "8:music"
-  , className =? "Movie Player" --> doShift "7:video" ]
+  [ className =? "Gimp" --> doShift "a:gimp"
+  , className =? "Empathy" --> doShift "b:chat"
+  , title     =? "Contact List" --> doShift "b:chat"
+  , className =? "Rhythmbox" --> doShift "c:media"
+  , className =? "Banshee Media Player" --> doShift "c:media"
+  , className =? "Movie Player" --> doShift "c:media" ]
 
-main = xmonad gnomeConfig
+-- http://www.haskell.org/haskellwiki/Xmonad/General_xmonad.hs_config_tips#Binding_Workspaces_on_Function_Keys
+myKeys = [ (otherModMasks ++ "M-" ++ key, action tag)
+         | (tag, key) <- zip myWorkspaces (map (\x -> "<F" ++ show x ++ ">") [1..12])
+         , (otherModMasks, action) <- [ ("", windows . W.greedyView) -- or W.view
+                                      , ("S-", windows . W.shift)]
+         ]
+
+main = xmonad $ gnomeConfig
   { workspaces = myWorkspaces
   , manageHook = manageDocks <+> myManageHook
-  , layoutHook = onWorkspace "9:gimp" gimp $ layoutHook gnomeConfig
+  , layoutHook = onWorkspace "a:gimp" gimp $ layoutHook gnomeConfig
   , terminal   = "xterm"
-  }
+  } `additionalKeysP` myKeys
   where
     gimp = withIM (0.18) (Role "gimp-toolbox") $
            reflectHoriz $
