75e1fd025f67017b14e0304e8e841655c9162ffc
Tim Baumann <tim@timbaumann.info>
2011-10-30 16:27:02 +0100

    Getting started with emacs and emacs-starter-kit

---
+++ .emacs.d/init.el
@@ -0,0 +1,16 @@
+;; packages
+
+(require 'package)
+(add-to-list 'package-archives
+             '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(defvar my-packages '(starter-kit starter-kit-lisp starter-kit-js starter-kit-ruby starter-kit-bindings)
+  "A list of packages to ensure are installed at launch.")
+
+(dolist (p my-packages)
+  (when (not (package-installed-p p))
+    (package-install p)))
--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ ln -s ~/dotfiles/.hgrc ~/.hgrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.emacs ~/.emacs
+ln -s ~/dotfiles/.emacs.d ~/.emacs.d
 ln -s ~/dotfiles/.viper ~/.viper
 ln -s ~/dotfiles/bin ~/bin
 ln -s ~/dotfiles/.xmonad ~/.xmonad
