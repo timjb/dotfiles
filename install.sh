3f69a97d4f216fef3a998e8cf79b016fd63cf0a1
Tim Baumann <tim@tim-thinkpad-arch.localdomain>
2012-02-27 18:26:02 +0100

    rm emacs dotfiles

--- install.sh
+++ install.sh
@@ -6,7 +6,5 @@ ln -s ~/dotfiles/.hgrc ~/.hgrc
 ln -s ~/dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/.vim ~/.vim
 ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dotfiles/.emacs.d ~/.emacs.d
-ln -s ~/dotfiles/.viper ~/.viper
 ln -s ~/dotfiles/bin ~/bin
 ln -s ~/dotfiles/.xmonad ~/.xmonad
