6fb6b9715d4960b6c24debe80df8d82945e93adc
Tim J. Baumann <tim@timbaumann.info>
2018-03-21 10:49:12 +0100

    Add node, perl env variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -34,15 +34,27 @@ add-to-path /usr/texbin
 add-to-path /sbin /usr/sbin
 add-to-path ~/Software/emsdk_portable/{,clang/e1.35.0_64bit,node/4.1.1_64bit/bin,emscripten/1.35.0}
 add-to-path ~/.cargo/bin
+add-to-path ~/.aloft/cmd
 
 # Haskell
 add-to-path ~/.local/bin # stack
-add-to-path "$HOME/.cabal/bin" "$HOME/.stack/programs/x86_64-osx/ghc-8.0.2/bin"
+#add-to-path "$HOME/.cabal/bin" "$HOME/.stack/programs/x86_64-osx/ghc-8.0.2/bin"
 add-to-path "$HOME/.cabal/bin" "$HOME/.stack/programs/x86_64-osx/ghc-8.2.1/bin"
 
 set -x DYLD_LIBRARY_PATH $DYLD_LIBRARY_PATH /usr/local/mysql/lib
 set -x DOCKER_HOST tcp://127.0.0.1:4243
 
+# Node
+set -x NODE_PATH "/usr/local/lib/node_modules"
+
+# Perl
+set -x PATH /Users/tim/perl5/bin $PATH ^/dev/null;
+set -q PERL5LIB; and set -x PERL5LIB /Users/tim/perl5/lib/perl5:$PERL5LIB;
+set -q PERL5LIB; or set -x PERL5LIB /Users/tim/perl5/lib/perl5;
+set -q PERL_LOCAL_LIB_ROOT; and set -x PERL_LOCAL_LIB_ROOT /Users/tim/perl5:$PERL_LOCAL_LIB_ROOT;
+set -q PERL_LOCAL_LIB_ROOT; or set -x PERL_LOCAL_LIB_ROOT /Users/tim/perl5;
+set -x PERL_MB_OPT --install_base\ \"/Users/tim/perl5\";
+set -x PERL_MM_OPT INSTALL_BASE=/Users/tim/perl5;
 
 function reload-config -d "Reload fish config file (this file)"
   . (status --current-filename)
@@ -50,9 +62,9 @@ end
 
 # Git aliases
 alias gst "git status"
-alias ga  "git add"
-alias gaa "git add --all"
-alias gc  "git commit"
+#alias ga  "git add"
+#alias gaa "git add --all"
+#alias gc  "git commit"
 
 # One letter aliases
 alias q exit
@@ -72,3 +84,7 @@ set -x LC_ALL "en_US.UTF-8"
 
 # Nix configuration
 source ~/.config/fish/nix.fish
+
+# OPAM configuration
+. ~/.opam/opam-init/init.fish
+eval (opam config env)
