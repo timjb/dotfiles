50d424bdd50959fade09e22d1e541d31989f11bf
Tim J. Baumann <tim@timbaumann.info>
2019-02-21 15:03:10 +0100

    Update VSCode extensions, install nix extension, sort alphabetically

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -76,28 +76,10 @@ in
         with unstable.vscode-extensions; [ ms-vscode.cpptools ] ++
         unstable.vscode-utils.extensionsFromVscodeMarketplace [
           {
-            name = "vscode-arduino";
-            publisher = "vsciot-vscode";
-            version = "0.2.24";
-            sha256 = "15bardalmrhvly2rr5qisb9b4ay0imxrrxmp97x79lli3msqq5gl";
-          }
-          {
-            name = "vim";
-            publisher = "vscodevim";
-            version = "0.16.12";
-            sha256 = "07j5mm6ghrdbvwnlw3qap7276n74l41ww28xljffkiq0qmbhnr8i";
-          }
-          {
-            name = "latex-workshop";
-            publisher = "James-Yu";
-            version = "5.16.0";
-            sha256 = "0cl4nl9yp54xfclm1gnkrig2i9npnf069ca91gcvy6jzbmdza6jj";
-          }
-          {
-            name = "fish";
-            publisher = "TeddyDD";
-            version = "0.0.4";
-            sha256 = "1wzixi5wx3dj94174fybis0180jk9mw3c7xyr0sa1awqp4xdwkx6";
+            name = "agda";
+            publisher = "j-mueller";
+            version = "0.1.7";
+            sha256 = "09ix3pjaagr6aakshc8ybbipgdscr8ymdrqc4xxs6927x552yjsb";
           }
           {
             name = "bracket-pair-colorizer";
@@ -106,10 +88,10 @@ in
             sha256 = "0r3bfp8kvhf9zpbiil7acx7zain26grk133f0r0syxqgml12i652";
           }
           {
-            name = "agda";
-            publisher = "j-mueller";
-            version = "0.1.7";
-            sha256 = "09ix3pjaagr6aakshc8ybbipgdscr8ymdrqc4xxs6927x552yjsb";
+            name = "fish";
+            publisher = "TeddyDD";
+            version = "0.0.4";
+            sha256 = "1wzixi5wx3dj94174fybis0180jk9mw3c7xyr0sa1awqp4xdwkx6";
           }
           {
             name = "language-haskell";
@@ -118,10 +100,34 @@ in
             sha256 = "10jqj8qw5x6da9l8zhjbra3xcbrwb4cpwc3ygsy29mam5pd8g6b3";
           }
           {
+            name = "latex-workshop";
+            publisher = "James-Yu";
+            version = "5.16.0";
+            sha256 = "0cl4nl9yp54xfclm1gnkrig2i9npnf069ca91gcvy6jzbmdza6jj";
+          }
+          {
+            name = "nix";
+            publisher = "bbenoist";
+            version = "1.0.1";
+            sha256 = "0zd0n9f5z1f0ckzfjr38xw2zzmcxg1gjrava7yahg5cvdcw6l35b";
+          }
+          {
             name = "python";
             publisher = "ms-python";
-            version = "2018.12.1";
-            sha256 = "1cf3yll2hfililcwq6avscgi35caccv8m8fdsvzqdfrggn5h41h4";
+            version = "2019.1.0";
+            sha256 = "1jp9i0qxdbrw3jk4in9za9cmfyrd6ii1ilgyg2van3mkq6xpp92w";
+          }
+          {
+            name = "vim";
+            publisher = "vscodevim";
+            version = "0.16.12";
+            sha256 = "07j5mm6ghrdbvwnlw3qap7276n74l41ww28xljffkiq0qmbhnr8i";
+          }
+          {
+            name = "vscode-arduino";
+            publisher = "vsciot-vscode";
+            version = "0.2.24";
+            sha256 = "15bardalmrhvly2rr5qisb9b4ay0imxrrxmp97x79lli3msqq5gl";
           }
         ];
     })
