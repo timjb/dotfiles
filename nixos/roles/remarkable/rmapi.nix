3745116d1a7ffa9613a94af4dd915bad53a555ac
Tim J. Baumann <tim@timbaumann.info>
2019-09-15 20:09:34 +0200

    [nixos] Update rmapi

--- nixos/roles/remarkable/rmapi.nix
+++ nixos/roles/remarkable/rmapi.nix
@@ -1,18 +1,19 @@
-{ buildGoPackage, fetchFromGitHub, lib }:
+{ buildGoModule, fetchFromGitHub, lib }:
 
-buildGoPackage rec {
+buildGoModule rec {
   name = "rmapi-${version}";
-  version = "config-file-env-var-c01d07";
+  version = "master-70a4fe";
 
   src = fetchFromGitHub {
-    owner = "timjb";
+    owner = "juruen";
     repo = "rmapi";
-    rev = "c01d070d2c8fd438c143d3af9a2ea52c2c952a45";
-    sha256 = "0959gc284q5mm70yi0m7jd5pb1bb6hk47wija40nrqcz6gq4ll62";
+    rev = "70a4fef8b0ea6f87f17c3b0af2bb3b6bccaef1e1";
+    sha256 = "082h597b4mqfnb567j85mpkffw6q3vfi8d9dla7qf7fhb0327vjg";
   };
 
   goPackagePath = "github.com/juruen/rmapi";
   subPackages = [ "." ];
+  modSha256 = "0a22pfmn733hddayh5xi977gh3abzf70hck6s6gmvxq7i61plhpb";
 
   meta = with lib; {
     description = "Go app that allows you to access your reMarkable tablet files through the Cloud API";
