80dad0992b0793cbd715d883ca793a0585508d3d
Tim J. Baumann <tim@timbaumann.info>
2020-11-01 19:03:44 +0100

    [nixos] Use Rust packages from standard nixpkgs

--- nixos/roles/rust.nix
+++ nixos/roles/rust.nix
@@ -1,13 +1,10 @@
 { pkgs, ... }:
 
-let
-  pkgs-unstable = import <nixpkgs-unstable> { config = { allowUnfree = true; }; };
-in
 {
   environment.systemPackages = with pkgs; [
-    pkgs-unstable.cargo
-    pkgs-unstable.rustc
-    pkgs-unstable.rls
+    cargo
+    rustc
+    rls
   ];
   my-config.vscodeExtensions = with (import ../vscode-extensions.nix); [
     rust
