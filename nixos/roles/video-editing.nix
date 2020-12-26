c7668ba9f0aa4e74b8ddc4a70bcec8d55dac36de
Tim J. Baumann <tim@timbaumann.info>
2020-12-26 01:51:14 +0100

    [nixos] Remove workaround for missing frei0r plugins in Kdenlive
    it is not required anymore

--- nixos/roles/video-editing.nix
+++ nixos/roles/video-editing.nix
@@ -1,17 +1,6 @@
 { pkgs, ... }:
 
 {
-  # fix issue of missing frei0r plugins,
-  # see https://github.com/NixOS/nixpkgs/issues/29614#issuecomment-511118058
-  nixpkgs.config.packageOverrides = pkgs: {
-    kdenlive = pkgs.kdenlive.overrideAttrs (oldAttrs: rec {
-        postInstall = ''
-         wrapProgram $out/bin/kdenlive --prefix FREI0R_PATH : ${pkgs.frei0r}/lib/frei0r-1
-       '';
-       nativeBuildInputs = oldAttrs.nativeBuildInputs or [] ++ [ pkgs.makeWrapper ];
-    });
-  };
-
   environment.systemPackages = with pkgs; [
     (ffmpeg-full.override {
       nonfreeLicensing = true;
