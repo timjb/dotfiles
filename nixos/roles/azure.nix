322411e5d47b558c2652b8f29b6c9bc443ab5e30
Tim J. Baumann <tim@timbaumann.info>
2023-07-28 09:02:20 +0200

    [nixos] Install Azure Data Studio

--- nixos/roles/azure.nix
+++ nixos/roles/azure.nix
@@ -4,6 +4,7 @@
   environment.systemPackages = with pkgs; [
     nodejs
     azure-cli
+    azuredatastudio
   ];
   my-config.vscodeExtensions = with (import ../vscode-extensions.nix pkgs); [
     azure-account
