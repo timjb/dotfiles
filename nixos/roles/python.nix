86a0e6bc4859623a29f853b29756764c3dc167d1
Tim J. Baumann <tim@timbaumann.info>
2023-07-28 09:03:47 +0200

    [nixos] install pip

--- nixos/roles/python.nix
+++ nixos/roles/python.nix
@@ -3,6 +3,7 @@
 {
   environment.systemPackages = with pkgs; [
     (python39.withPackages (ps: with ps; [
+      pip
       pygments
       pylint # used by the Python VSCode extension
       virtualenvwrapper
