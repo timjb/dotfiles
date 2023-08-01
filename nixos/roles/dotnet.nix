5c2623d41b2495c80f3a3ac98e6fd4936de388f1
Tim J. Baumann <tim@timbaumann.info>
2023-08-01 10:35:42 +0200

    [nixos] Update to .NET 7

--- nixos/roles/dotnet.nix
+++ nixos/roles/dotnet.nix
@@ -4,11 +4,14 @@
   config = {
 
     environment.etc = {
-      "dotnet/install_location_x64".text = "${pkgs.dotnet-sdk}";
+      "dotnet/install_location_x64".text = "${pkgs.dotnet-sdk_7}";
+      # stable locations for configuring SDK in IDE
+      "dotnet6".source = "${pkgs.dotnet-sdk}";
+      "dotnet7".source = "${pkgs.dotnet-sdk_7}";
     };
 
     environment.systemPackages = with pkgs; [
-      dotnet-sdk
+      dotnet-sdk_7
       jetbrains.rider
       msbuild
     ];
