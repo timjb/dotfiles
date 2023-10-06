e9d2cf44166918b809989e9a3425ca1204f77393
Tim J. Baumann <tim@timbaumann.info>
2023-10-06 10:09:06 +0200

    [nixos] install rmapi from stable channel

--- nixos/roles/remarkable.nix
+++ nixos/roles/remarkable.nix
@@ -1,13 +1,12 @@
 { pkgs, ... }:
 
 let
-  unstable-channel = import <nixpkgs-unstable> {}; # use unstable because it contains v0.23, which has a fix for https://github.com/juruen/rmapi/issues/283
-  remarkable-cups = pkgs.callPackage ./remarkable/remarkable-cups.nix { rmapi = unstable-channel.rmapi; };
+  remarkable-cups = pkgs.callPackage ./remarkable/remarkable-cups.nix { rmapi = pkgs.rmapi; };
 in
 
 {
   environment.systemPackages = with pkgs; [
-    unstable-channel.rmapi
+    rmapi
   ];
 
   services.printing.enable = true;
