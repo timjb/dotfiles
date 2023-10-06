51ea253fc16aada026ccf3f6441f821f25ac2da7
Tim J. Baumann <tim@timbaumann.info>
2023-10-06 10:25:11 +0200

    [nixos] Install Azure Functions Core Tools

--- nixos/roles/azure.nix
+++ nixos/roles/azure.nix
@@ -4,6 +4,7 @@
   environment.systemPackages = with pkgs; [
     nodejs
     azure-cli
+    azure-functions-core-tools
     azuredatastudio
   ];
   my-config.vscodeExtensions = with (import ../vscode-extensions.nix pkgs); [
